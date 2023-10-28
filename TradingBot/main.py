d3090ca419d68b0b6033ee14d53190fa11db3c73
BGreezy <124118732+BGreezy@users.noreply.github.com>
2023-10-28 15:10:42 -0700

    Update main.py

--- TradingBot/main.py
+++ TradingBot/main.py
@@ -1,1 +1,4 @@
+from data_fetching import your_data_fetching_function
+from trading_strategies import your_trading_strategy_function
+from performance_metrics import your_performance_metrics_function
 
