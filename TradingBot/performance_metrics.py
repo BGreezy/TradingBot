6a5ad2171078a46ecdbd33e2aea8c8c47cda07d3
BGreezy <124118732+BGreezy@users.noreply.github.com>
2023-10-28 15:07:06 -0700

    Update performance_metrics.py

--- TradingBot/performance_metrics.py
+++ TradingBot/performance_metrics.py
@@ -1,1 +1,6 @@
 
+# Performance Metrics: Implementing a simple Sharpe Ratio calculation
+def calculate_sharpe_ratio(returns, risk_free_rate=0.01):
+    excess_returns = returns - risk_free_rate
+    return excess_returns.mean() / excess_returns.std()
+    # TODO: Add more performance metrics like Drawdown, Beta, and Alpha
